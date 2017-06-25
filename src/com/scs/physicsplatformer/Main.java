bb3fd05573fe39d1f8cfd36ddd6ef33997b5ebef
Stephen Carlyle-Smith <stephen.carlylesmith@googlemail.com>
2017-06-25 15:46:23 +0100

    Added screenshots

--- src/com/scs/physicsplatformer/Main.java
+++ src/com/scs/physicsplatformer/Main.java
@@ -51,7 +51,7 @@ public class Main implements ContactListener, NewControllerListener, KeyListener
 	private List<Contact> collisions = new LinkedList<>();
 	private AbstractLevel level;
 	private boolean restartLevel = false;
-	private int levelNum = 4;
+	private int levelNum = 1;
 	
 
 	public static void main(String[] args) {
